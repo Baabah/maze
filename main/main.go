dac933ba891b4ad177cc307fb5ce1c7dc6fba082
Peter Ton <baab1337@gmail.com>
2016-12-27 14:05:55 +0100

    Added multiple channels

--- main/main.go
+++ main/main.go
@@ -7,23 +7,35 @@ import (
 	"fmt"
 )
 
+// todo add solution model
+
 func main() {
 	var maze = generator.BuildMaze()
-	var solutions [][]model.Position
-	for i := 0; i < 5000; i++ {
-		solutions = append(solutions, solver.Solve(maze))
+	dataChannel := make(chan []model.Position)
+	for i := 0; i < 30; i++ {
+		go worker(maze, dataChannel)
 	}
+
 	var best []model.Position
-	for _, solution := range solutions {
+	for {
+		solution := <- dataChannel
 		if best == nil {
 			best = solution
+			fmt.Println(best)
+			fmt.Println("Best solution is:", len(best))
 			continue
 		}
 		if len(solution) < len(best) {
 			best = solution
+			fmt.Println(best)
+			fmt.Println("Best solution is:", len(best))
 		}
 	}
-	fmt.Println(best)
-	fmt.Println("Best solution is:", len(best))
+}
 
+func worker(maze model.Maze, dataChannel chan []model.Position) {
+	for {
+		solution := solver.Solve(maze)
+		dataChannel <- solution
+	}
 }
